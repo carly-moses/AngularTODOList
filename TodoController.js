21d9b9359de5686e00c414316d57fb279bff345b
Carly Moses <cmoses916@gmail.com>
2018-05-21 21:49:51 -0400

    Lab Part 2 Added functionality

--- TodoController.js
+++ TodoController.js
@@ -4,15 +4,53 @@ function ToDoInfo(){
     const vm = this;
     vm.listitems = 
         [
-            {task: "Walk the dog", completed: true},
+            {task: "Walk the dog", completed: false},
             {task: "Go to the store", completed: false},
             {task: "Get Gas", completed: false},
-            {task: "Wash the car", completed: true}
+            {task: "Wash the car", completed: false},
         ];
 
-    vm.removeItem = (index) => {
+    vm.removeTask = (index) => {
         vm.listitems.splice(index, 1);
     };
+
+    vm.addTask = (task) => {
+        vm.listitems.push({
+            task: task,
+            completed: false
+        });
+        vm.listitems.task = ""; 
+    };
+
+    vm.completeTask = (index, completed) => {
+        vm.listitems[index].completed = true; 
+    };
+
+    vm.inComplete = (index, completed) => { 
+        vm.listitems[index].completed = false; 
+        vm.tempIndex = index;   
+        vm.showForm = false; 
+    };
+
+
+    vm.editTask = (index, info) => {
+        vm.showForm = true; 
+            vm.tempTask = {
+                task: info.task,
+        }
+        vm.tempIndex = index;   
+    };
+
+    vm.updateTask = (index, info) => {
+        vm.listitems.splice(index, 1, {
+            task: info.task,
+            completed: false
+        });
+        vm.showForm = false; 
+    
+    };
+
+
 
 };
 
